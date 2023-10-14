41e66cbccc6c91b57a7bd81e1e5899094d5206f5
Thiago Martins Reis <devthiagomartins@gmail.com>
2023-10-14 09:37:01 -0300

    Projeto da loja virtual criado

---
+++ src/main/java/com/nexus/nexuslojavirtual/NexusLojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.nexus.nexuslojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusLojaVirtualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusLojaVirtualApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nexus/nexuslojavirtual/NexusLojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.nexuslojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusLojaVirtualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
