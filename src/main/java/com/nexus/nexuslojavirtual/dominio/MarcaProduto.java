07a46ac951d2c0d4e1379aaed4704b51050e7be4
Thiago Martins Reis <devthiagomartins@gmail.com>
2023-10-16 19:17:21 -0300

    classe MarcaProduto

---
+++ src/main/java/com/nexus/nexuslojavirtual/dominio/MarcaProduto.java
@@ -0,0 +1,32 @@
+package com.nexus.nexuslojavirtual.dominio;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
